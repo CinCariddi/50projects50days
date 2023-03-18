f2ff9bccd1b9ab19400bf2b5a67089ec6f13f1be
CinCariddi <cintiacariddi@gmail.com>
2023-03-18 18:41:05 -0300

    Dia 4

--- src/Days/Day4.jsx
+++ src/Days/Day4.jsx
@@ -1,5 +1,29 @@
+import './Day4.css'
+import { useState } from 'react'
+
 export default function Day4() {
+    const [search, setSearch] = useState(false)
+
+    const handleClick = () => {
+        if(search === true){
+            setSearch(false)
+        }else {
+            setSearch(true)
+        }
+    }
+
     return(
-        <div>oli</div>
+        <div className='body4'>
+            <div className='search'>
+                {
+                    search === true ? 
+                    <input type="text" className='inputDia4' placeholder="Search..."/>
+                    : <div></div>
+                }
+                <button className="btnDia4" onClick={() => handleClick()}>
+                    <i>🔍︎</i>
+                </button>
+            </div>
+        </div>
     )
 }
