99ec3438201768796c55a4795d440bf8cba191db
CinCariddi <cintiacariddi@gmail.com>
2023-04-10 22:32:13 -0300

    Dia 27

--- src/Days/Day27.jsx
+++ src/Days/Day27.jsx
@@ -5,8 +5,9 @@ export default function Day27() {
     const button = useRef("button");
     const toasts = useRef("toasts");
     const noti = useRef(null);
+    const [notif, setNotif] = useState([])
 
-    console.log("notificacion", noti);
+    console.log("noti", noti);
 
     const [messages /*setMessages*/] = useState([
         "Message One",
@@ -17,25 +18,28 @@ export default function Day27() {
     const [types /*setTypes*/] = useState(["info", "success", "error"]);
 
     function createNotification(message = null, type = null) {
-        noti.current.classList.add(type ? type : getRandomType());
-        noti.current.classList.add("toast");
-        noti.current.innerText = message ? message : getRandomMessage();
-        toasts.current.append(noti.current);
-        console.log("noti", noti);
-        setTimeout(() => {
-        noti.current.remove();
-        }, 3000);
+        if(notif.length >= 0){
+            noti.current.classList.add(type ? type : getRandomType());
+            noti.current.classList.add("toasts");
+            noti.current.innerText = message ? message : getRandomMessage();
+            toasts.current.append(noti.current);
+            setTimeout(() => {
+                noti.current.remove();
+            }, 3000);
+    
+            setNotif([...notif, noti.current.innerText])
+        }
+
+        console.log("notificacion", notif);
     }
 
     function getRandomMessage() {
         const mes = messages[Math.floor(Math.random() * 3)];
-        console.log("mensaje", mes);
         return mes;
     }
 
     function getRandomType() {
         const tipo = types[Math.floor(Math.random() * 2)];
-        console.log("tipo", tipo);
         return tipo;
     }
 
@@ -45,12 +49,18 @@ export default function Day27() {
 
     return (
         <div className="contenedorDia27">
-        <div ref={toasts}>
-            <div ref={noti}></div>
-        </div>
-        <button className="btn" ref={button} onClick={handleClick}>
-            Show Notification
-        </button>
+            <div ref={toasts}>
+                <div ref={noti}>
+                    {
+                    notif?.map((n) => (
+                        n
+                    ))
+                    }
+                </div>
+            </div>
+            <button className="btn27" ref={button} onClick={handleClick}>
+                Show Notification
+            </button>
         </div>
     );
 }
