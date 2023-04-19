2fe8f4571bae820398d8222706c368feb2191537
CinCariddi <cintiacariddi@gmail.com>
2023-04-19 15:36:34 -0300

    Dia 31

--- src/Days/Day31.jsx
+++ src/Days/Day31.jsx
@@ -1,123 +1,99 @@
 import "./Day31.css";
-import React, {useEffect} from 'react';
+import React, {useState} from 'react';
 
 export default function Day31() {
-  const resultEl = document.getElementById("result");
-  const lengthEl = document.getElementById("length");
-  const uppercaseEl = document.getElementById("uppercase");
-  const lowercaseEl = document.getElementById("lowercase");
-  const numbersEl = document.getElementById("numbers");
-  const symbolsEl = document.getElementById("symbols");
-  const generateEl = document.getElementById("generate");
-  const clipboardEl = document.getElementById("clipboard");
+  const [password, setPassword] = useState('');
+  const [passLength, setPassLength] = useState(20);
+  const [hasLower, setHasLower] = useState(false);
+  const [hasUpper, setHasUpper] = useState(false);
+  const [hasNumber, setHasNumber] = useState(false);
+  const [hasSymbol, setHasSymbol] = useState(false);
 
   const randomFunc = {
     lower: getRandomLower,
     upper: getRandomUpper,
     number: getRandomNumber,
     symbol: getRandomSymbol
-  };
+  }
 
-  clipboardEl.addEventListener("click", () => {
-    const password = resultEl.innerText;
+  function generatePassword(lower, upper, number, symbol, passLength) {
+    let generatedPassword = ''
+    const typesCount = lower + upper + number + symbol
+    const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0])
+    if(typesCount === 0) {
+      return ''
+    }
+    for(let i = 0; i < passLength; i += typesCount) {
+      typesArr.forEach(type => {
+        const funcName = Object.keys(type)[0]
+        generatedPassword += randomFunc[funcName]()
+      })
+    }
+    const finalPassword = generatedPassword.slice(0, passLength)
+    return finalPassword
+  }
+
+  function handleGeneratePassword() {
+    const generatedPassword = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, passLength);
+    setPassword(generatedPassword);
+  }
+
+  function handleCopyToClipboard() {
     if (!password) {
       return;
     }
     navigator.clipboard.writeText(password);
-    alert("Password copied to clipboard!");
-  });
-
-  useEffect(() => {
-      generateEl.addEventListener("click", () => {
-        const length = +lengthEl.value;
-        const hasLower = lowercaseEl.checked;
-        const hasUpper = uppercaseEl.checked;
-        const hasNumber = numbersEl.checked;
-        const hasSymbol = symbolsEl.checked;
-    
-        resultEl.innerText = generatePassword(
-          hasLower,
-          hasUpper,
-          hasNumber,
-          hasSymbol,
-          length
-        );
-      });
-
-  })
-
-  function generatePassword(lower, upper, number, symbol, length) {
-    let generatedPassword = "";
-    const typesCount = lower + upper + number + symbol;
-    const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(
-      (item) => Object.values(item)[0]
-    );
-
-    if (typesCount === 0) {
-      return "";
-    }
-
-    for (let i = 0; i < length; i += typesCount) {
-      typesArr.forEach((type) => {
-        const funcName = Object.keys(type)[0];
-        generatedPassword += randomFunc[funcName]();
-      });
-    }
-
-    const finalPassword = generatedPassword.slice(0, length);
-
-    return finalPassword;
+    alert('Password copied to clipboard!');
   }
 
   function getRandomLower() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
   }
-
+  
   function getRandomUpper() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
   }
-
+  
   function getRandomNumber() {
-    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
   }
-
+  
   function getRandomSymbol() {
-    const symbols = "!@#$%^&*(){}[]=<>/,.";
+    const symbols = '!@#$%^&'
     return symbols[Math.floor(Math.random() * symbols.length)];
   }
+
   return (
     <div className="contenedorDia31">
-      <div class="container">
+      <div className="container">
         <h2>Password Generator</h2>
-        <div class="result-container">
-          <span id="result"></span>
-          <button class="btn" id="clipboard">
-            <i class="far fa-clipboard"></i>
-          </button>
+        <div className="result-container">
+          <span>{password}</span>
+          <button onClick={handleCopyToClipboard} className="btn btn-large"></button>
         </div>
         <div class="settings">
           <div class="setting">
             <label>Password Length</label>
-            <input type="number" id="length" min="4" max="20" value="20" />
+            <input type="number" id="length" min="4" max="20"  value={passLength} onChange={(e) => setPassLength(e.target.value)}/>
           </div>
           <div class="setting">
             <label>Include uppercase letters</label>
-            <input type="checkbox" id="uppercase" checked />
+            <input type="checkbox" id="uppercase" checked={hasUpper} onChange={(e) => setHasUpper(e.target.checked)}/>
           </div>
           <div class="setting">
             <label>Include lowercase letters</label>
-            <input type="checkbox" id="lowercase" checked />
+            <input type="checkbox" id="lowercase" checked={hasLower} onChange={(e) => setHasLower(e.target.checked)} />
           </div>
           <div class="setting">
             <label>Include numbers</label>
-            <input type="checkbox" id="numbers" checked />
+            <input type="checkbox" id="numbers" checked={hasNumber} onChange={(e) => setHasNumber(e.target.checked)}/>
           </div>
           <div class="setting">
             <label>Include symbols</label>
-            <input type="checkbox" id="symbols" checked />
+            <input type="checkbox" id="symbols" checked={hasSymbol} onChange={(e) => setHasSymbol(e.target.checked)} />
           </div>
         </div>
-        <button class="btn btn-large" id="generate">
+        <button class="btn btn-large" onClick={handleGeneratePassword}>
           Generate Password
         </button>
       </div>
